0dde7ad991f2d686d831aafce66783de5c17b1ea
Philipp Schilter <philipp.schilter@screenconcept.ch>
2009-12-30 16:53:04 +0100

    added navigation type

--- navigation_tags_extension.rb
+++ navigation_tags_extension.rb
@@ -9,9 +9,22 @@ class NavigationTagsExtension < Radiant::Extension
   # define_routes do |map|
   #   map.connect 'admin/navigation_tags/:action', :controller => 'admin/navigation_tags'
   # end
-  
+ 
+  define_routes do |map|
+    map.namespace :admin, :member => { :remove => :get } do |admin|
+      admin.resources :navigation_types
+    end
+  end
+    
   def activate
     Page.send :include, NavigationTags
+    
+ #   %w{page}.each do |view|
+#      admin.send(view).edit.add "Navigation Type", "/admin/navigation_types", :after => "Layouts", :visibility => [:all]
+#      admin.send(view).edit.add :parts_bottom, "navigationType", :after  => 'edit_layout_and_type' 
+#    end
+    admin.tabs.add "Navigation Type", "/admin/navigation_types", :after => "Layouts", :visibility => [:all]
+    admin.page.edit.add :parts_bottom, "navigationType", :after  => 'edit_layout_and_type'    
   end
   
   def deactivate
